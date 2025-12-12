ff3b60d2921a2ebeb6383c6c8839c605f2d81e91
Malmeu <darkvid3r@gmail.com>
2025-12-12 13:45:44 +0100

    fix: use www.malmeu.info as canonical URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://malmeu.info',
+  site: 'https://www.malmeu.info',
   output: 'server',
   adapter: vercel({
     imageService: true
